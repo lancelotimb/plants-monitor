e7f169a1d121d75f6041db0c83ded332a1d930c6
lancelotimb <lancelot.imb@gmail.com>
2022-01-28 20:01:09 +0100

    Refactor python code

--- arduino-querier.py
+++ arduino-querier.py
@@ -7,11 +7,11 @@ import serial, time
 import sqlite3
 
 # Arduino Response Messages
-RESPONSE_UNKNOWN_COMMAND = "UNKNOWN_COMMAND";
-RESPONSE_HUMIDITY_MEASUREMENT = "HUMIDITY $value";
+RESPONSE_UNKNOWN_COMMAND = "UNKNOWN_COMMAND"
+RESPONSE_HUMIDITY_MEASUREMENT = "HUMIDITY $value"
 
 # Arduino Command Messages
-COMMAND_GET_HUMIDITY = "GET_HUMIDITY";
+COMMAND_GET_HUMIDITY = "GET_HUMIDITY"
 
 
 def create_connection():
@@ -21,7 +21,7 @@ def create_connection():
         conn = sqlite3.connect(database)
     except Error as e:
         print(e)
-        
+
     return conn
 
 
@@ -36,7 +36,7 @@ def record_measurement(connection, data):
 def interpret_answer(connection, message):
     print(message)
     split_message = message.split()
-    
+
     if split_message[0] == RESPONSE_HUMIDITY_MEASUREMENT.split()[0] :
         measurement_data = split_message[1]
         record_measurement(connection, measurement_data)
@@ -45,14 +45,14 @@ def interpret_answer(connection, message):
 def debug():
     # Initialize database
     connection = create_connection()
-    
+
     # Launch connection with Arduino
     with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
-        
+
         # Wait for serial to open
         time.sleep(0.1)
-        
-        
+
+
         if arduino.isOpen():
             print("Connected! (Port {})".format(arduino.port))
             try:
@@ -60,17 +60,17 @@ def debug():
                     cmd = input("Enter command : ")
                     print(cmd.encode())
                     arduino.write(cmd.encode())
-                    
+
                     # time.sleep(0.1) #wait for arduino to answer
                     while arduino.inWaiting() == 0:
                         pass
-                    
+
                     if arduino.inWaiting() > 0:
-                        answer = arduino.readline().decode("utf-8")   
+                        answer = arduino.readline().decode("utf-8")
                         interpret_answer(connection, answer)
-                        
+
                         # Remove data after reading
-                        arduino.flushInput() 
+                        arduino.flushInput()
             except KeyboardInterrupt:
                 print("KeyboardInterrupt has been caught.")
 
@@ -78,31 +78,31 @@ def debug():
 def main():
     # Initialize database
     connection = create_connection()
-    
+
     # Launch connection with Arduino
     with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
         arduino.reset_input_buffer()
-        
+
         # Wait for serial to open
         time.sleep(0.1)
-        
-        
+
+
         if arduino.isOpen():
             try:
                 time.sleep(3) # Needed for connection to initialize?
                 while True:
                     arduino.write(COMMAND_GET_HUMIDITY.encode())
-                    
+
                     while arduino.inWaiting() == 0:
                         pass
-                    
+
                     if arduino.inWaiting() > 0:
-                        answer = arduino.readline().decode("utf-8").rstrip()  
+                        answer = arduino.readline().decode("utf-8").rstrip()
                         interpret_answer(connection, answer)
-                        
+
                         # Remove data after reading
                         arduino.flushInput()
-                    
+
                     time.sleep(10) # 10s between each data point
             except KeyboardInterrupt:
                 print("KeyboardInterrupt has been caught.")
