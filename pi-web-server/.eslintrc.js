a708b0af732ce10b5dcfa155ad6e7f17cd7dc695
lancelotimb <lancelot.imb@gmail.com>
2022-01-30 12:25:09 +0100

    Fix indent

--- pi-web-server/.eslintrc.js
+++ pi-web-server/.eslintrc.js
@@ -1,33 +1,33 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb-base',
-    'airbnb-typescript/base',
-    'plugin:import/typescript',
-    'prettier'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json'
-  },
-  plugins: [
-    '@typescript-eslint',
-    'prettier'
-  ],
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
-      }
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'airbnb-base',
+        'airbnb-typescript/base',
+        'plugin:import/typescript',
+        'prettier'
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json'
+    },
+    plugins: [
+        '@typescript-eslint',
+        'prettier'
+    ],
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx']
+            }
+        }
+    },
+    rules: {
+        'prettier/prettier': 'error',
+        'import/prefer-default-export': 'off',
     }
-  },
-  rules: {
-    'prettier/prettier': 'error',
-    'import/prefer-default-export': 'off',
-  }
 };
