1ad1284c22592d703c7efdaec16db28341b434df
lancelotimb <lancelot.imb@gmail.com>
2022-01-30 14:24:50 +0100

    Add first api hook

---
+++ plants-monitor-frontend/components/IndexPage/IndexPage.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { useHumidityMeasurements } from 'lib/api/humidity-measurements';
+
+export const IndexPage: React.FunctionComponent = () => {
+    const { data, isLoading, isError } = useHumidityMeasurements();
+
+    if (isError) return <div>Failed to load</div>;
+    if (isLoading) return <div>Loading...</div>;
+    return <div>{data}</div>;
+};
---
+++ plants-monitor-frontend/lib/api/humidity-measurements.ts
@@ -0,0 +1,16 @@
+import useSWR from 'swr';
+
+const API_ROUTE_HUMIDITY_MEASUREMENTS = '/humidity-measurements';
+
+export const fetchHumidityMeasurements = () =>
+    fetch(API_ROUTE_HUMIDITY_MEASUREMENTS).then((res) => res.json());
+
+export const useHumidityMeasurements = () => {
+    const { data, error } = useSWR('get-humidity-measurements', () => fetchHumidityMeasurements());
+
+    return {
+        data,
+        isLoading: !error && !data,
+        isError: error,
+    };
+};
