770890ab0a70f9890968c09da137f51ada294c52
lancelotimb <lancelot.imb@gmail.com>
2022-02-04 20:35:22 +0100

    Add support fot DHT sensor

--- arduino-server/arduino-server.ino
+++ arduino-server/arduino-server.ino
@@ -1,12 +1,14 @@
+#include "DHT.h"
+
 #define NB_SENSORS 5
+#define DHTTYPE DHT11
 
 String msg;
 
 int SENSOR_PINS[NB_SENSORS] = {A0, A1, A2, A3, A4};
-String SENSOR_IDS[NB_SENSORS] = {"hum1", "hum2", "hum3", "hum4", "hum5"};
-String PUMP_IDS[] = {"pump1", "pump2", "pump3", "pump4"};
 
 String COMMAND_GET_HUMIDITY = "GET_HUMIDITY";
+String COMMAND_GET_DHT = "GET_DHT";
 String COMMAND_ACTIVATE_PUMP_1 = "ACTIVATE_PUMP_1";
 String COMMAND_ACTIVATE_PUMP_2 = "ACTIVATE_PUMP_2";
 String COMMAND_ACTIVATE_PUMP_3 = "ACTIVATE_PUMP_3";
@@ -14,10 +16,17 @@ String COMMAND_ACTIVATE_PUMP_4 = "ACTIVATE_PUMP_4";
 
 String RESPONSE_UNKNOWN_COMMAND = "UNKNOWN_COMMAND";
 String RESPONSE_HUMIDITY_MEASUREMENT = "HUMIDITY $value";
+String RESPONSE_DHT_MEASUREMENT = "DHT $value";
 
+// Declare DHT sensor
+int DHT_SENSOR_PIN = 2;
+DHT dht(DHT_SENSOR_PIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
+
+  // Initialize DHT sensor
+  dht.begin();
 
   // Initialize Humidity Sensors
   for (int i = 0; i < NB_SENSORS; i++) {
@@ -51,17 +60,25 @@ void sendData(String message) {
 
 String read_humidity() {
   String data = "[";
-    for (int i = 0; i < NB_SENSORS; i++) {
-      String value = String(analogRead(SENSOR_PINS[i]));
-      data += value;
+  for (int i = 0; i < NB_SENSORS; i++) {
+    String value = String(analogRead(SENSOR_PINS[i]));
+    data += value;
 
-      if (i < NB_SENSORS - 1) {
-        data += ",";
-      }
+    if (i < NB_SENSORS - 1) {
+      data += ",";
     }
-    data += "]";
+  }
+  data += "]";
 
-    return data;
+  return data;
+}
+
+String read_dht() {
+  Serial.println("Temperature = " + String(dht.readTemperature()) + " °C");
+  Serial.println("Humidite = " + String(dht.readHumidity()) + " %");
+
+  String data = "{temperature:" + String(dht.readTemperature()) + ", humidity:" + String(dht.readHumidity()) + "}";
+  return data;
 }
 
 String executeCommand(String command) {
@@ -70,7 +87,12 @@ String executeCommand(String command) {
     response.replace("$value", read_humidity());
     return response;
   }
-  else {
-    return RESPONSE_UNKNOWN_COMMAND;
+
+  if (command == COMMAND_GET_DHT) {
+    String response = RESPONSE_DHT_MEASUREMENT;
+    response.replace("$value", read_dht());
+    return response;
   }
+
+  return RESPONSE_UNKNOWN_COMMAND;
 }
