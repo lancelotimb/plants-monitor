93b591cb6941306c0b4f64e3dbc998309dc21fa7
lancelotimb <lancelot.imb@gmail.com>
2022-02-12 12:12:51 +0100

    Test port 80

--- pi-web-server/index.ts
+++ pi-web-server/index.ts
@@ -1,18 +1,31 @@
-import app from './app';
+import express from 'express';
+import cors, { CorsOptionsDelegate } from 'cors';
+import humidityMeasurementsRouter from './src/routes/humidityMeasurements.route';
+import dhtMeasurementsRouter from './src/routes/dhtMeasurements.route';
 
-require('greenlock-express')
-    .init({
-        packageRoot: './',
+const app = express();
+const port = 80;
 
-        // contact for security and critical bug notices
-        maintainerEmail: 'lancelot.imb@gmail.com',
+app.use(cors());
+const CORS_WHITE_LIST = ['http://localhost:3000', 'https://plants-monitor.vercel.app']; // TODO move to .env file
+const corsOptionsDelegate: CorsOptionsDelegate = (req, callback) => {
+    const origin = req.headers.Origin;
+    if (origin && typeof origin === 'string' && CORS_WHITE_LIST.indexOf(origin) !== -1) {
+        // Enable the requested origin in the CORS response
+        callback(null, { origin: true });
+    } else {
+        // Disable CORS for this request
+        callback(null, { origin: false });
+    }
+};
 
-        // where to look for configuration
-        configDir: './greenlock.d',
+app.get('/', (req, res) => {
+    res.json({ message: 'ok' });
+});
 
-        // whether or not to run at cloudscale
-        cluster: false,
-    })
-    // Serves on 80 and 443
-    // Get's SSL certificates magically!
-    .serve(app);
+app.use('/humidity-measurements', cors(corsOptionsDelegate), humidityMeasurementsRouter);
+app.use('/dht-measurements', cors(corsOptionsDelegate), dhtMeasurementsRouter);
+
+app.listen(port, '0.0.0.0');
+
+export default app;
